3ef4c5c7d599d4b9119230f26a49758ae5d2abd7
Irina <weedfox053@gmail.com>
2025-08-31 17:42:32 +0300

    React Tabs

--- src/components/Tabs/Tabs.jsx
+++ src/components/Tabs/Tabs.jsx
@@ -1,1 +1,27 @@
-export const Tabs = () => {};
+export const Tabs = ({ tabs, activeTabId, onTabSelected }) => {
+  const active = tabs.find(j => j.id === activeTabId) || tabs[0];
+
+  return (
+    <div>
+      <ul>
+        {tabs.map(i => (
+          <li
+            data-cy="Tab"
+            key={i.id}
+            className={i.id === activeTabId ? 'is-active' : ''}
+          >
+            <a data-cy="TabLink" href={`#tab-${i.id}`}
+             onClick={(e) => onTabSelected(i.id)
+             }>
+              <h1>{i.title}</h1>
+            </a>
+          </li>
+        ))}
+      </ul>
+      
+      <div data-cy="TabContent">
+      {active.content}
+    </div>
+    </div>
+  );
+};
